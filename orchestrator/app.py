84e131779b226eab3d8cb4d34ff4b63f1faba6b7
ErikaGM19 <erika.garcia.munoz@correounivalle.edu.co>
2025-11-05 17:23:46 -0500

    subiendo cambios

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -9,6 +9,7 @@ SERVICES = [
     {"name":"inventory","url": os.getenv("INVENTORY_URL","http://inventory_service:5103")},
     {"name":"order","url": os.getenv("ORDER_URL","http://order_service:5104")},
     {"name":"notification","url": os.getenv("NOTIF_URL","http://notification_service:5107")},
+    {"name":"billing","url": os.getenv("BILLING_URL","http://billing_service:5106")},
 ]
 
 TIMEOUT = 5
